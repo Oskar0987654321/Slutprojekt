26ed8c14eebc270739da43048df75a883d48abce
Oskar0987654321 <129067570+Oskar0987654321@users.noreply.github.com>
2023-05-12 17:37:30 +0200

    Nu har jag gjort så att man först väljer vilken rad och sedan vilken av rutorna på den raden, sedan har jag gjort så att den säger till om man väljer en ruta som redan är upptagen och att man då får välja en ny ruta, sedan har jag gjort så att om någon vinner så printar den "Wins!" och om det blir lika så printar den "Tie!" och nu är spelet färdigt.

--- rictactoe.py
+++ rictactoe.py
@@ -33,5 +33,31 @@ def tic_tac_toe():
     player2 = 'O'
     current_player = player1
     
+    while True:
+        print_board()
+        
+        row = int(input("Enter row number (1-3): ")) - 1
+        col = int(input("Enter colum number (1-3): ")) - 1
+        
+        if board[row][col] != '-':
+            print("That cell is already occupied. Try again.")
+            continue 
+            
+        board[row][col] = current_player
+        
+        if check_win(current_player):
+            print_board()
+            print(current_player, "wins!")
+            break
     
+        if all('-' not in row for row in board):
+            print_board()
+            print("Tie!")
+            break
+            
+        if current_player == player1:
+            current_player = player2
+        else: 
+            current_player = player1
+tic_tac_toe()
     
