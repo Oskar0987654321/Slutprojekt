4693e2a6b14304fa48ed8f12a832571f4c43fc97
Oskar0987654321 <129067570+Oskar0987654321@users.noreply.github.com>
2023-05-12 16:31:00 +0200

    Jag tog bort allt som jag hade gjort för jag tyckte inte att den spelplanen såg bra ut, sedan gjorde jag en ny spelplan, gjorde så att de tre raderna printas och sedan skrev jag in så att spelet kollar alla möjliga vinstkombinationer och om det är tre i rad så blir det True och om det inte är en vinst så blir det False

--- rictactoe.py
+++ rictactoe.py
@@ -1,9 +1,32 @@
-import numpy as np
-import os
-import time
+board = [
+    ['-', '-', '-'],
+    ['-', '-', '-'],
+    ['-', '-', '-']
+]
 
-def creata_grid():
-    board = [[" ", " ", " "],
-             [" ", " ", " "],
-             [" ", " ", " "]]
-    return board
+def print_board(): 
+    for row in board:
+        print(row[0], row[1], row[2])
+        
+def checkwin(player):
+    if board[0][0] == player and board[0][1] == player and board[0][2] == player:
+        return True
+    elif board[1][0] == player and board[1][1] == player and board[1][2] == player:
+        return True
+    elif board[2][0] == player and board[2][1] == player and board [2][2] == player:
+        return True
+    elif board[0][0] == player and board[1][0] == player and board[2][0] == player:
+        return True
+    elif board[0][1] == player and board[1][1] == player and board[2][1] == player:
+        return True
+    elif board[0][2] == player and board[1][2] == player and board[2][2] == player:
+        return True
+    elif board[0][0] == player and board[1][1] == player and board[2][2] == player:
+        return True
+    elif board[0][2] == player and board[1][1] == player and board[2][0] == player:
+        return True
+    else:
+        return False
+    
+    
+    
